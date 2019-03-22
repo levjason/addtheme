c4b5e3d233cc83c60608316a30f20bcea3dd3646
root <root@workspace2231rs1xj1r5vscj.ws-5b6d89c79c-fpq9q>
2019-03-22 19:17:14 +0000

    Import ThemeService

--- src/addtheme-frontend.ts
+++ src/addtheme-frontend.ts
@@ -4,6 +4,8 @@
  */
 
 import * as theia from '@theia/plugin';
+import { ThemeService } from '@theia/core/lib/browser/theming';
+import { injectable, inject } from 'inversify';
 
 export function start(context: theia.PluginContext) {
     console.log(theia.window.state);
