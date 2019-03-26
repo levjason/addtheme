2f274bec97a359e8692c0cf3edee53203e698658
root <root@workspace2231rs1xj1r5vscj.ws-5b6d89c79c-fpq9q>
2019-03-26 18:28:36 +0000

    obtain ThemeService

--- src/addtheme-frontend.ts
+++ src/addtheme-frontend.ts
@@ -5,12 +5,21 @@
 
 import * as theia from '@theia/plugin';
 import { ThemeService } from '@theia/core/lib/browser/theming';
-import { injectable, inject } from 'inversify';
+
+
+
 
 export function start(context: theia.PluginContext) {
+     
+    const themeService = ThemeService.get();
+    const currentTheme = themeService.getCurrentTheme();
+    console.log(currentTheme.label);
+
+
     console.log(theia.window.state);
 }
 
 export function stop() {
 
+}
 }
