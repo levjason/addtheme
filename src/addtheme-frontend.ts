6fb1e0d39b0fa4a8f250c99fa96f96e9dbbe21f3
root <root@workspace2231rs1xj1r5vscj.ws-5b6d89c79c-fpq9q>
2019-03-26 19:21:08 +0000

    Added command

--- src/addtheme-frontend.ts
+++ src/addtheme-frontend.ts
@@ -10,16 +10,20 @@ import { ThemeService } from '@theia/core/lib/browser/theming';
 
 
 export function start(context: theia.PluginContext) {
-     
-    const themeService = ThemeService.get();
-    const currentTheme = themeService.getCurrentTheme();
-    console.log(currentTheme.label);
 
 
-    console.log(theia.window.state);
+
+    const informationMessageTestCommand = {
+        id: 'addtheme-example-generated',
+        label: "Add a theme"
+    };
+    context.subscriptions.push(theia.commands.registerCommand(informationMessageTestCommand, (...args: any[]) => {
+        const themeService = ThemeService.get();
+        const currentTheme = themeService.getCurrentTheme();
+        theia.window.showInformationMessage(currentTheme.label);
+    }));
 }
 
 export function stop() {
 
-}
 }
